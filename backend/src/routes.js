df9e3694f6b539c477fe22f267efa842f5c3c605
Matheus Santos <dev.santos@outlook.com>
2020-04-13 17:25:46 -0300

    backend fix bugs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -9,16 +10,46 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+//Rota para login na aplicação
 routes.post('/sessions', SessionController.create);
 
+//Rota para listagem das ongs
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
+//Rota pra crição das ongs
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    }),
+}), OngController.create);
 
-routes.get('/incidents', IncidentController.index);
+//Rota para alguma cooisa
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+//Rota para listagem dos incidentes
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}), IncidentController.index);
+
+//Rota para criação dos incidentes
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+//Rota para deletar os incidentes
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 
 module.exports = routes;
